d2e2495e2a378c6885b76243e8069d23d3c9d199
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 12:48:18 +0500

    Add: 10) Cheksiz loop xavfsiz variant [mazgi976]

---
+++ lms_kod_topshiriqlari/Infinite_loop_tushunchasi_va_oldini_olish/10__Cheksiz_loop_xavfsiz_variant.py
@@ -0,0 +1,11 @@
+while True:
+    tanlov = int(input())
+        
+    if tanlov == 0:
+        break
+    elif tanlov == 1:
+        print("1-variant")
+    elif tanlov == 2:
+        print("2-variant")
+    elif tanlov == 3:
+        print("3-variant")
