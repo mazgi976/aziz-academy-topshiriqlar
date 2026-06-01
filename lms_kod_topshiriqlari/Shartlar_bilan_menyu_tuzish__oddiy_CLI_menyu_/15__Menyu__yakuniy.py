8f1ebc5756dbed1415f4c0f83c27008066c59261
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 12:31:49 +0500

    Add: 15) Menyu: yakuniy [mazgi976]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/15__Menyu__yakuniy.py
@@ -0,0 +1,12 @@
+choice = input()
+if choice == "start":
+    print("Started")
+elif choice == "stop":
+    print("Stopped")
+elif choice == "pause":
+    print("Paussed")
+else:
+    print("Unknown command")
+
+
+
