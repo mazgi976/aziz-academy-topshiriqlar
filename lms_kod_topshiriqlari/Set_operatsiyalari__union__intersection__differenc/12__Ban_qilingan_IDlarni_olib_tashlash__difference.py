99d60ca6a753e0ce175548674109cb0ac22f3133
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 21:47:04 +0500

    Add: 12) Ban qilingan IDlarni olib tashlash (difference) [mazgi976]

---
+++ lms_kod_topshiriqlari/Set_operatsiyalari__union__intersection__differenc/12__Ban_qilingan_IDlarni_olib_tashlash__difference.py
@@ -0,0 +1,9 @@
+ids = set(map(int, input().split()))
+banned = set(map(int, input().split()))
+_ = input()  
+result = ids - banned 
+
+if result:
+    print(*sorted(result))
+else:
+    print("BO'SH")
