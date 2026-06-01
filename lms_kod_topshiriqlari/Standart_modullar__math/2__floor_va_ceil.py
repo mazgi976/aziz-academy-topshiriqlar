d23302f81ecb3062b7bd7e5dcf343a09ad77308c
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 09:16:18 +0500

    Add: 2) floor va ceil [mazgi976]

---
+++ lms_kod_topshiriqlari/Standart_modullar__math/2__floor_va_ceil.py
@@ -0,0 +1,13 @@
+import math
+
+def main():
+    x = float(input())
+    
+    fl = math.floor(x)
+    cl = math.ceil(x)
+    
+    print(fl)
+    print(cl)
+    
+if __name__ == '__main__':
+    main()
