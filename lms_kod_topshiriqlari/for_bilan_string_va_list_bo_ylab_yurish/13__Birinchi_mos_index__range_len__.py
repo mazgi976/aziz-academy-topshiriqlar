468caabc440b8f92d400622b7010cd6c53e60280
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 12:36:32 +0500

    Add: 13) Birinchi mos index (range(len)) [mazgi976]

---
+++ lms_kod_topshiriqlari/for_bilan_string_va_list_bo_ylab_yurish/13__Birinchi_mos_index__range_len__.py
@@ -0,0 +1,13 @@
+n = int(input())
+lst = list(map(int, input().split()))
+x = int(input())
+
+index = -1
+
+for i in range(n):
+    if lst[i] == x:
+        index = i
+        break
+        
+print(index)        
+        
