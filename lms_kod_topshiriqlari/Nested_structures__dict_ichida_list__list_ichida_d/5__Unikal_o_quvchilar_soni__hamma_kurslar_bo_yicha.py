c24c85f0103a9f7317042d30c929138fabf3c2b2
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 22:53:08 +0500

    Add: 5) Unikal o‘quvchilar soni (hamma kurslar bo‘yicha) [mazgi976]

---
+++ lms_kod_topshiriqlari/Nested_structures__dict_ichida_list__list_ichida_d/5__Unikal_o_quvchilar_soni__hamma_kurslar_bo_yicha.py
@@ -0,0 +1,14 @@
+n = int(input().strip())
+courses = []
+for _ in range(n):
+    parts = input().split()
+    name = parts[0]
+    k = int(parts[1])
+    students = parts[2:2+k]
+    courses.append({'name': name, 'students': students})
+
+unique_students = set()
+for course in courses:
+    unique_students.update(course['students'])
+
+print(len(unique_students))
