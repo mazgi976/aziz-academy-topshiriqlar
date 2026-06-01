080728d3961a38e75e6ed590a575582d93423b01
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 22:50:02 +0500

    Add: 13) Eng arzon mahsulot nomi [mazgi976]

---
+++ lms_kod_topshiriqlari/List_of_dicts__o_quvchilar_va_mahsulotlar/13__Eng_arzon_mahsulot_nomi.py
@@ -0,0 +1,8 @@
+n = int(input())
+products = []
+for _ in range(n):
+    name, price = input().split()
+    products.append({'name': name, 'price': int(price)})
+
+cheapest_product = min(products, key=lambda p: p['price'])
+print(cheapest_product['name'])
