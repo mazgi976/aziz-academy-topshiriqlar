4793f1932e6c752215087c9036a8903801314da7
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 22:49:36 +0500

    Add: 4) Eng yaxshi o‘quvchi nomi [mazgi976]

---
+++ lms_kod_topshiriqlari/List_of_dicts__o_quvchilar_va_mahsulotlar/4__Eng_yaxshi_o_quvchi_nomi.py
@@ -0,0 +1,8 @@
+n = int(input())
+students = []
+for _ in range(n):
+    name, score = input().split()
+    students.append({'name': name, 'score': int(score)})
+
+top_student = max(students, key=lambda s: s['score'])  
+print(top_student['name'])
