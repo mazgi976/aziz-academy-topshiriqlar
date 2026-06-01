6105776d19ee4c70044d5035e2c9088a4f1c720a
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 12:45:53 +0500

    Add: 9) Foydalanuvchi chiqishni tanlashi [mazgi976]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/9__Foydalanuvchi_chiqishni_tanlashi.py
@@ -0,0 +1,14 @@
+s = 3
+while True:
+    g = int(input())
+    if g == 0:
+        print("Exit")
+        break
+    if g < s: print("kichik")
+    elif g > s: print("katta")
+    else:
+        print("to'g'ri")
+        break
+    
+
+
