58f6aa415e4c4b3bed2dabd9fd727b7bba9bce58
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-01 09:24:11 +0500

    Add: 15) Yakuniy: Quadratic formula (diskriminant) [mazgi976]

---
+++ lms_kod_topshiriqlari/Standart_modullar__math/15__Yakuniy__Quadratic_formula__diskriminant_.py
@@ -0,0 +1,22 @@
+import math
+
+def main():
+    a, b, c = map(float, input().split())
+    
+    D = b * b - 4 * a * c
+    if D < 0:
+        print("NO")
+    elif D == 0:
+        x = -b / (2 * a)
+        print(f"{x:.4f}")
+    else:
+        x1 = (-b - math.sqrt(D)) / (2 * a)
+        x2 = (-b + math.sqrt(D)) / (2 * a)
+        
+        if x1 > x2:
+            x1, x2 = x2, x1
+            
+        print(f"{x1:.4f} {x2:.4f}")
+        
+if __name__ == '__main__':
+    main()
