e31736c06c532bf37b66f600ff70efa0b4ae8fab
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-02 13:08:43 +0500

    Add: 7) Saralash: Name | Score (score desc, name asc) [mazgi976]

---
+++ lms_kod_topshiriqlari/Kichik__hisobot__konsolda_chiqarish__table_ko_rini/7__Saralash__Name___Score__score_desc__name_asc_.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+students = []
+
+for _ in range(n):
+    name, score = input().split()
+    score = int(score)
+    
+    students.append((name, score))
+    
+students.sort(key=lambda x: (-x[1], x[0]))
+
+print(f"{'Name':<10} | {'Score':>5}")
+print("----------+-----")
+
+for name, score in students:
+    print(f"{name:<10} | {score:>5}")
