8be1f323cfc7f77cb93583264d105747dd4ae870
mazgi976 <shukurullokarimov27@gmail.com>
2026-06-27 11:12:46 +0500

    Add AI task: Avto servisda ishlatiladigan benzin miqdorini hisoblovchi dastur

---
+++ ai_topshiriqlar/Avto_servisda_ishlatiladigan_benzin_miqdorini_hisoblovchi_da.py
@@ -0,0 +1,17 @@
+# Avto servisda ishlatiladigan benzin miqdorini hisoblovchi dastur
+# Kurs: Dasturlash / IT
+# Mavzu: Type casting ⭐ — type(), int(), float(), str(), bool()
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+data = input().split()
+h = int(data[0])
+i = int(data[1])
+t = int(data[2])
+
+res = (i * t) / h * 100
+
+if res == int(res):
+    print(int(res))
+else:
+    print(res)
