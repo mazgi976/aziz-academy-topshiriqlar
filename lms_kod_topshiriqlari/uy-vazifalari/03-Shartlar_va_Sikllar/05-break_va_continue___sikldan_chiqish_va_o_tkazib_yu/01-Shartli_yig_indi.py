658feca604cbcbebbfcaa52e9ee8d187614e21b5
mazgi976 <shukurullokarimov27@gmail.com>
2026-07-03 10:25:22 +0500

    Add: Shartli yig'indi [mazgi976]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Shartli_yig_indi.py
@@ -0,0 +1,13 @@
+yigindi = 0
+
+while True:
+    n = int(input())
+    
+    if n == 0 or n > 100:
+        break
+        
+    if n < 0:
+        continue
+        
+    yigindi += n
+print(yigindi)
