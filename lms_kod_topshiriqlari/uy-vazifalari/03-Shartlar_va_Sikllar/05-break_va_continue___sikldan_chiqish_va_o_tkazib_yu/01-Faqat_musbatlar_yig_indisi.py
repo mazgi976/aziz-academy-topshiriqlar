41f541d43d84132d7cb3274725ef307ee6e0eeac
mazgi976 <shukurullokarimov27@gmail.com>
2026-07-03 10:23:16 +0500

    Add: Faqat musbatlar yig'indisi [mazgi976]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/05-break_va_continue___sikldan_chiqish_va_o_tkazib_yu/01-Faqat_musbatlar_yig_indisi.py
@@ -0,0 +1,14 @@
+n = int(input())
+yigindi = 0
+sanoq = 0
+
+while sanoq < n:
+    son = int(input())
+    sanoq += 1
+    
+    if son <= 0:
+        continue
+        
+    yigindi += son
+    
+print(yigindi)
