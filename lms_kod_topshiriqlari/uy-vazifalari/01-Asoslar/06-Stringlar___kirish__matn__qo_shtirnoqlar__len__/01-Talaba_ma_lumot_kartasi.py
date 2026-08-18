19c1336c6a627df79818577e2e40e537ec68b287
mazgi976 <shukurullokarimov27@gmail.com>
2026-08-18 13:09:01 +0500

    Add: Talaba ma'lumot kartasi [mazgi976]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/06-Stringlar___kirish__matn__qo_shtirnoqlar__len__/01-Talaba_ma_lumot_kartasi.py
@@ -0,0 +1,9 @@
+ism = "Aziz Karimov"
+kurs = "Python"
+shahar = "Toshkent"
+
+print("=== KARTA ===")
+print(f"Ism: {ism}")
+print(f"Kurs: {kurs}")
+print(f"Shahar: {shahar}")
+print(f"Ism uzunligi: {len(ism)}")
