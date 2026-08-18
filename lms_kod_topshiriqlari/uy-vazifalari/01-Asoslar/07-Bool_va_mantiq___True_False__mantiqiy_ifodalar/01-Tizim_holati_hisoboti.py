922afa10d1c25c6e917167724caf11ba9178d6ae
mazgi976 <shukurullokarimov27@gmail.com>
2026-08-18 13:18:20 +0500

    Add: Tizim holati hisoboti [mazgi976]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Tizim_holati_hisoboti.py
@@ -0,0 +1,10 @@
+server_ishlayapti = True
+baza_ulangan = True
+xatolik_bor = False
+xavfsiz = True
+
+print("=== TIZIM HOLATI ===")
+print(f"Server ishlayapti: {server_ishlayapti}")
+print(f"Baza ulangan: {baza_ulangan}")
+print(f"Xatolik bor: {xatolik_bor}")
+print(f"Xavfsiz: {xavfsiz}")
