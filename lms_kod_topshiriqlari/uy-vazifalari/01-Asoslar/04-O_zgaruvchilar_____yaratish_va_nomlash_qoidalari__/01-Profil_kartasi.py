099cb3dc7510481f6245e053fc6d3f171951d0e0
mazgi976 <shukurullokarimov27@gmail.com>
2026-08-18 12:20:04 +0500

    Add: Profil kartasi [mazgi976]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -0,0 +1,12 @@
+ism = "Aziz"
+familiya = "Karimov"
+kurs = "Python"
+daraja = "Boshlang'ich"
+shahar = "Toshkent"
+
+print("=== PROFIL ===")
+print("Ism:", ism)
+print("Familiya:", familiya)
+print("Kurs:", kurs)
+print("Daraja:", daraja)
+print("Shahar:", shahar)
