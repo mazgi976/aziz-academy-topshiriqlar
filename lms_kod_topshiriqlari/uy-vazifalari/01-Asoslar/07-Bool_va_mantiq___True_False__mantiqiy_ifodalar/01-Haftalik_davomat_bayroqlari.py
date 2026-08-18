94e729eba8cc77b31297960ed97b99b31a939128
mazgi976 <shukurullokarimov27@gmail.com>
2026-08-18 13:19:02 +0500

    Add: Haftalik davomat bayroqlari [mazgi976]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Haftalik_davomat_bayroqlari.py
@@ -0,0 +1,11 @@
+dushanba = True
+seshanba = True
+chorshanba = False
+payshanba = True
+juma = False
+
+print(f"Dushanba: {dushanba}")
+print(f"Seshanba: {seshanba}")
+print(f"Chorshanba: {chorshanba}")
+print(f"Payshanba: {payshanba}")
+print(f"Juma: {juma}")
