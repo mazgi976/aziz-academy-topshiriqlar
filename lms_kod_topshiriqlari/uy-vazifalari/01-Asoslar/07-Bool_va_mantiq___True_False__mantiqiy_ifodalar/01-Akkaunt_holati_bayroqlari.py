78cd274d18f53b3fb6a5558aa3f6cc1c91fc274f
mazgi976 <shukurullokarimov27@gmail.com>
2026-08-19 21:45:23 +0500

    Add: Akkaunt holati bayroqlari [mazgi976]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/07-Bool_va_mantiq___True_False__mantiqiy_ifodalar/01-Akkaunt_holati_bayroqlari.py
@@ -0,0 +1,7 @@
+royxatdan_otgan = True
+email_tasdiqlangan = False
+bloklangan = False
+
+print(f"Ro'yxatdan o'tgan: {royxatdan_otgan}")
+print(f"Email tasdiqlangan: {email_tasdiqlangan}")
+print(f"Bloklangan: {bloklangan}")
