7b451178e33db0efee7ed057f53c34d4b78644cf
mazgi976 <shukurullokarimov27@gmail.com>
2026-08-23 17:47:12 +0500

    Add: Email va parol validatsiyasi [mazgi976]

--- lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/25-__Mini-loyiha__Login___parol_tekshirish__validatsi/01-Email_va_parol_validatsiyasi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/02-Stringlar_va_Formatlash/25-__Mini-loyiha__Login___parol_tekshirish__validatsi/01-Email_va_parol_validatsiyasi.py
@@ -1,8 +1,4 @@
 email = input()
 parol = input()
 
-shart1 = "@" in email and "." in email
-shart2 = 8 <= len(parol) <= 16
-shart3 = email == email.lower()
-
-print(shart1 and shart2 and shart3)
+print('@' in email and '.' in email and 8 <= len(parol) <= 16 and email == email.lower())
