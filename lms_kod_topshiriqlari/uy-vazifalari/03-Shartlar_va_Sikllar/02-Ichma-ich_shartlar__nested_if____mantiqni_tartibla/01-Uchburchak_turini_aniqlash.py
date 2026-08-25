aed306e15dc3d08dff26eb4caa30cfb8071e7023
mazgi976 <shukurullokarimov27@gmail.com>
2026-08-25 16:34:01 +0500

    Add: Uchburchak turini aniqlash [mazgi976]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Uchburchak_turini_aniqlash.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/02-Ichma-ich_shartlar__nested_if____mantiqni_tartibla/01-Uchburchak_turini_aniqlash.py
@@ -5,7 +5,7 @@ c = int(input())
 if (a + b > c) and (a + c > b) and (b + c > a):
     if a == b == c:
         print("Teng tomonli")
-    elif a == b or a == c or b == c:
+    elif a == b or b == c or a == c:
         print("Teng yonli")
     else:
         print("Turli tomonli")
