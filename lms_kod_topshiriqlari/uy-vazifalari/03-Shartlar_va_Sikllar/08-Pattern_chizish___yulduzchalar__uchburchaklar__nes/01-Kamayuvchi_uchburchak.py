44afcff1bcf983126bd537199bcee20c501e3fe0
mazgi976 <shukurullokarimov27@gmail.com>
2026-09-01 10:26:53 +0500

    Add: Kamayuvchi uchburchak [mazgi976]

--- lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/08-Pattern_chizish___yulduzchalar__uchburchaklar__nes/01-Kamayuvchi_uchburchak.py
+++ lms_kod_topshiriqlari/uy-vazifalari/03-Shartlar_va_Sikllar/08-Pattern_chizish___yulduzchalar__uchburchaklar__nes/01-Kamayuvchi_uchburchak.py
@@ -1,4 +1,16 @@
-n = int(input())
+import sys
 
-for i in range(n, 0, -1):
+
+def solve():
+  input_data = sys.stdin.read().split()
+  if not input_data:
+    return
+
+  n = int(input_data[0])
+    
+  for i in range(n, 0, -1):
     print("*" * i)
+
+
+if __name__ == "__main__":
+  solve()
